ba25c6dd727455e176f06ae85302f613c07f7dba
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-09-29 18:54:32 -0300

    Updated button ui component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,11 @@
-const Button = ({ children, ...props }) => {
+const Button = ({ children, color, ...props }) => {
   return (
     <button
-      className='bg-light-1 text-dark-1 min-w-[100px] rounded-md font-semibold py-2 mt-4 xl:text-xl 2xl:text-2xl 2xl:py-3'
+      className={`${
+        color === 'inverted'
+          ? 'bg-dark-2 text-light-1'
+          : 'bg-light-1 text-dark-1'
+      } min-w-[100px] rounded-md font-semibold py-2 mt-4 lg:text-lg xl:text-xl 2xl:text-2xl 2xl:py-3`}
       {...props}
     >
       {children}
