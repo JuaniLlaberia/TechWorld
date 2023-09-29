bd09e7f53f4c99a292aabf1188ed8fdf86f8fd41
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-09-29 18:54:11 -0300

    Added basic structure of homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,40 @@
-import JobList from '../components/JobList';
+import {
+  HiOutlineGlobeAlt,
+  HiOutlineNewspaper,
+  HiOutlineBriefcase,
+} from 'react-icons/hi2';
+import IconCard from '../components/IconCard';
+import JobPreviewList from '../features/jobs/JobPreviewList';
 
 const Home = () => {
   return (
     <>
-      <h1 className='text-light-1 font-semibold'>Popular jobs</h1>
-      <JobList />
+      {true ? (
+        <h1 className='text-light-1 text-2xl font-semibold mb-4'>
+          Welcome back, John!
+        </h1>
+      ) : (
+        <div className='bg-[red]'>SIGN UP TO ACCESS</div>
+      )}
+      <section className='grid grid-cols-3 gap-2 mb-2 mt-3'>
+        <IconCard
+          icon={<HiOutlineBriefcase />}
+          text='All jobs'
+          path='/jobs'
+        />
+        <IconCard
+          icon={<HiOutlineNewspaper />}
+          text='Recent'
+          path='/jobs'
+        />
+        <IconCard
+          icon={<HiOutlineGlobeAlt />}
+          text='Jobs map'
+          path='/map'
+        />
+      </section>
+      <h2 className='text-light-2 text-lg font-semibold mb-2'>Jobs for you</h2>
+      <JobPreviewList />
     </>
   );
 };
