02a7ea26bce2537502f66476d2ea5f7d8616a982
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-01 20:51:01 -0300

    Updated profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,12 +3,12 @@ import Modal from '../components/Modal';
 import UpdateDescForm from '../features/users/UpdateDescForm';
 import UpdateListForm from '../features/users/UpdateListForm';
 import ItemsList from '../components/ItemsList';
-import ProfileCard from '../components/ProfileCard';
+import Card from '../components/Card';
 import UpdateProfileForm from '../features/users/UpdateProfileForm';
 import ExperienceItem from '../features/users/ExperienceItem';
 
 import { EditBtn } from '../components/EditBtn';
-import { useGetMe } from '../hooks/useGetMe';
+import { useGetMe } from '../features/users/useGetMe';
 import { SkillItem } from '../features/users/SkillItem';
 import UpdateShowExperiences from '../features/users/UpdateShowExperiences';
 
@@ -18,12 +18,19 @@ const Profile = () => {
   if (isLoading) return <h1 className='text-light-1'>Loading...</h1>;
   if (user.status === 'fail') return null;
 
-  const { fullName, profession, description, image, skills, experience } =
-    user.data || user;
+  const {
+    fullName,
+    profession,
+    description,
+    image,
+    skills,
+    experience,
+    location,
+  } = user.data || user;
 
   return (
     <Modal>
-      <ProfileCard position='horizontal'>
+      <Card position='horizontal'>
         <img
           src={defaultUserImg}
           className='rounded-full w-28 lg:w-36'
@@ -32,15 +39,16 @@ const Profile = () => {
           <h1 className='text-light-1 text-xl font-bold lg:text-3xl'>
             {fullName}
           </h1>
-          <h2 className='text-light-2 text-base font-semibold lg:text-xl'>
+          <h2 className='text-light-2 font-semibold lg:text-xl'>
             {profession}
           </h2>
+          <h2 className='text-light-2 text-sm lg:text-xl'>{location}</h2>
         </div>
         <Modal.Open opens='personal-info-modal'>
           <EditBtn />
         </Modal.Open>
-      </ProfileCard>
-      <ProfileCard>
+      </Card>
+      <Card>
         <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>About</h3>
         <p className='text-light-1 text-sm text-justify lg:text-xl'>
           {description}
@@ -48,8 +56,8 @@ const Profile = () => {
         <Modal.Open opens='about-modal'>
           <EditBtn />
         </Modal.Open>
-      </ProfileCard>
-      <ProfileCard>
+      </Card>
+      <Card>
         <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>
           Experience & Projects
         </h3>
@@ -70,8 +78,8 @@ const Profile = () => {
         <Modal.Open opens='experience-modal-form'>
           <EditBtn />
         </Modal.Open>
-      </ProfileCard>
-      <ProfileCard>
+      </Card>
+      <Card>
         <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>Skills</h3>
         <ItemsList
           items={skills.slice(0, 5)}
@@ -91,11 +99,11 @@ const Profile = () => {
         <Modal.Open opens='skills-modal'>
           <EditBtn />
         </Modal.Open>
-      </ProfileCard>
+      </Card>
 
       <Modal.Window windowName='personal-info-modal'>
         <UpdateProfileForm
-          current={{ fullName, profession }}
+          current={{ fullName, profession, location }}
           image={defaultUserImg}
         />
       </Modal.Window>
