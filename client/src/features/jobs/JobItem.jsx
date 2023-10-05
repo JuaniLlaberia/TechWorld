ca2519bd38d380c7f28650be054302cea5376bad
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-05 17:10:26 -0300

    Updated job item component

--- client/src/features/jobs/JobItem.jsx
+++ client/src/features/jobs/JobItem.jsx
@@ -3,25 +3,29 @@ import defaultUserImg from '/default.jpg';
 import { HiOutlineBookmark } from 'react-icons/hi2';
 
 const JobItem = ({ item }) => {
-  const { _id, name, img, company, location } = item;
+  const { _id, name, user, location, workPlace } = item;
 
   return (
     <li className='relative border-b-[1px] border-dark-1-border last:border-0'>
       <Link
         to={`/job/${_id}`}
-        className='text-light-1 flex gap-4 py-2'
+        className='text-light-1 flex flex-col gap-4 py-2 w-full'
       >
-        <img
-          src={defaultUserImg}
-          className='w-20 rounded-full mb-2 xl:w-24'
-        />
-        <div className=' w-full'>
+        <div className='w-full'>
           <h3 className='text-lg font-semibold break-keep lg:text-xl 2xl:text-[1.35rem]'>
             {name}
           </h3>
-          <h4 className='text-light-2 lg:text-lg'>{company}</h4>
           <h4 className='text-light-2 text-sm lg:text-base xl:text-lg'>
-            {location}
+            {location} ({workPlace})
+          </h4>
+        </div>
+        <div className='flex items-center gap-2 mb-1'>
+          <img
+            src={defaultUserImg}
+            className='w-8 h-8 rounded-full xl:w-10'
+          />
+          <h4 className='text-light-2 text-sm font-semibold lg:text-base'>
+            {user?.fullName}
           </h4>
         </div>
       </Link>
