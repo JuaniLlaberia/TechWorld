5894527a9f7d7f1b52f3271beaa80af6bc98a9c9
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-06 13:21:09 -0300

    Updated jobs controller

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -132,6 +132,6 @@ exports.getJobsFromUser = catchErrorAsync(async (req, res) => {
 
 //Get all the jobs posted by me
 exports.getMyJobs = catchErrorAsync(async (req, res) => {
-  const jobs = await Job.find({ user: req.user.id });
+  const jobs = await Job.find({ user: req.user.id }).select('name');
   res.status(200).json({ status: 'success', count: jobs.length, data: jobs });
 });
