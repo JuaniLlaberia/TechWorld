6c956e4ad498344ab57a7dabdfaea96550ce9831
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-09 15:55:00 -0300

    Updated save posts functionality

--- client/src/components/SavePost.jsx
+++ client/src/components/SavePost.jsx
@@ -1,25 +1,36 @@
 import { HiOutlineBookmark, HiBookmark } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 import { useSaveJob } from '../features/jobs/useSaveJob';
 import { useUnSaveJob } from '../features/jobs/useUnSaveJob';
-import { useAuthContext } from '../context/AuthContext';
 
 const SavePost = ({ id }) => {
-  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const { user, isAuth } = useAuthContext();
   const { save, isSaving } = useSaveJob();
   const { unSave, isUnSaving } = useUnSaveJob();
 
-  const isPostSave = user.data.savedPosts.includes(id);
+  const isPostSave = user?.data?.savedPosts.includes(id);
 
   return (
     <button
-      className={`absolute right-0 bottom-6 z-10 text-light-1 ${
+      className={`z-10 text-light-1 ${
         isSaving || isUnSaving ? 'animate-pulse' : ''
       }`}
     >
       {isPostSave ? (
-        <HiBookmark size={30} onClick={() => unSave(id)} />
+        <HiBookmark
+          size={30}
+          onClick={() => unSave(id)}
+        />
       ) : (
-        <HiOutlineBookmark size={30} onClick={() => save(id)} />
+        <HiOutlineBookmark
+          size={30}
+          onClick={() =>
+            !isAuth && !isPostSave ? navigate('/signup') : save(id)
+          }
+        />
       )}
     </button>
   );
