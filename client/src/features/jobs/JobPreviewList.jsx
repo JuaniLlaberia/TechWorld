14ba25fc6203865b65dc171b8c8044c0005cb74d
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-10 20:04:03 -0300

    Updated job preview structure

--- client/src/features/jobs/JobPreviewList.jsx
+++ client/src/features/jobs/JobPreviewList.jsx
@@ -1,23 +1,28 @@
-import { Link } from 'react-router-dom';
 import { useGetJobs } from './useGetJobs';
 import JobList from './JobList';
+import { Link } from 'react-router-dom';
 
 const JobPreviewList = ({ query, showAmount = 5, link }) => {
   const { jobs, isLoading } = useGetJobs(query);
 
   return (
-    <section className='bg-dark-2 p-2 rounded-md '>
-      <JobList
-        isLoading={isLoading}
-        jobs={jobs?.data?.jobs.slice(0, showAmount)}
-      />
-      <Link
-        to={link || '/jobs/all'}
-        className='text-light-2 flex justify-center py-1.5 border-t-[1px] border-dark-1-border lg:text-xl lg:py-3'
-      >
-        View more
-      </Link>
-    </section>
+    <>
+      <div className='flex items-center justify-between mb-3 mt-6'>
+        <h2 className='text-light-2 text-lg font-semibold'>Recent jobs</h2>
+        <Link
+          to='/jobs/all'
+          className='text-light-2'
+        >
+          View more
+        </Link>
+      </div>
+      <section className='bg-dark-2 px-3 rounded-md '>
+        <JobList
+          isLoading={isLoading}
+          jobs={jobs?.data?.jobs.slice(0, showAmount)}
+        />
+      </section>
+    </>
   );
 };
 
