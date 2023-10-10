c09952cc9dbb1fea4a2279afef141a01fd83c32d
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-10 20:06:04 -0300

    Created new home header

--- client/src/components/HomeHeader.jsx
+++ client/src/components/HomeHeader.jsx
@@ -1,14 +1,29 @@
+import { HiOutlineCog8Tooth } from 'react-icons/hi2';
 import { useAuthContext } from '../context/AuthContext';
 
 const HomeHeader = () => {
   const { user } = useAuthContext();
 
   return (
-    <h1 className='text-light-1 text-2xl font-semibold'>
-      {user?.status === 'fail'
-        ? 'Hey there!'
-        : `Welcome ${user?.data?.fullName?.split(' ')[0]}!`}
-    </h1>
+    <>
+      {user?.status === 'fail' ? (
+        <h1 className='text-light-1 text-lg font-semibold'>Welcome</h1>
+      ) : (
+        <>
+          <h1 className='mb-3'>
+            <span className='text-light-2 text-base'>Hello,</span>
+            <br />
+            <span className='text-light-1 text-2xl font-semibold'>
+              {user?.data?.fullName}
+            </span>
+          </h1>
+          <h6 className='text-light-1 absolute top-2.5 right-2.5 flex gap-2'>
+            <HiOutlineCog8Tooth size={30} />
+            <span className='hidden xl:block'>Settings </span>
+          </h6>
+        </>
+      )}
+    </>
   );
 };
 
