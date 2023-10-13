e50db24dd5552bc9265fca45481276f7e7e6439f
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-13 20:54:08 -0300

    Restyled nav bar

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -1,13 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
-const NavItem = ({ icon, path, text }) => {
+const NavItem = ({ iconActive, iconInactive, path, text }) => {
   return (
     <li className='text-light-2 relative hover:text-light-1 transition-colors'>
       <NavLink
         to={path}
         className='text-3xl lg:text-4xl 2xl:flex 2xl:items-center 2xl:gap-2'
       >
-        {icon} <span className='hidden text-2xl 2xl:block'>{text}</span>
+        {({ isActive }) =>
+          isActive ? (
+            <>
+              {iconActive}{' '}
+              <span className='hidden text-2xl 2xl:block'>{text}</span>
+            </>
+          ) : (
+            <>
+              {iconInactive}{' '}
+              <span className='hidden text-2xl 2xl:block'>{text}</span>
+            </>
+          )
+        }
       </NavLink>
     </li>
   );
