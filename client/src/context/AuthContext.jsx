7b12c050bc61ce00d7be51a9a603abf67e6428c5
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-15 19:43:36 -0300

    Added loader screen to auth

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -2,19 +2,14 @@ import { createContext, useContext } from 'react';
 import { useGetMe } from '../features/users/useGetMe';
 import { ClipLoader } from 'react-spinners';
 import { createPortal } from 'react-dom';
+import FullScreenLoader from '../components/FullScreenLoader';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const { isLoading, user: crrUser } = useGetMe();
 
-  if (isLoading)
-    return createPortal(
-      <section className='bg-dark-1 fixed top-0 h-full w-full flex justify-center items-center'>
-        <ClipLoader size={60} color='white' />
-      </section>,
-      document.body
-    );
+  if (isLoading) return createPortal(<FullScreenLoader />, document.body);
 
   const user = crrUser.status ? crrUser : { status: 'success', data: crrUser };
 
