3b4cf5ae2714605b108d42e3a2f9ed1f9ca7a8d7
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-15 13:50:01 -0300

    Added conditionally rendering of profile cards

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -21,7 +21,10 @@ export const UserProfile = () => {
       <BackBtn />
       <Modal>
         <Card position='horizontal'>
-          <img src={defaultUserImg} className='rounded-full w-28 lg:w-36' />
+          <img
+            src={defaultUserImg}
+            className='rounded-full w-28 lg:w-36'
+          />
           <div>
             <h1 className='text-light-1 text-xl font-bold lg:text-3xl'>
               {fullName}
@@ -36,44 +39,75 @@ export const UserProfile = () => {
           <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>About</h3>
           <p className='text-light-1 text-sm lg:text-xl'>{description}</p>
         </Card>
-        <Card>
-          <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>
-            Experience & Projects
-          </h3>
-          <ItemsList
-            items={experience?.slice(0, 3)}
-            render={el => <ExperienceItem el={el} key={el._id} />}
-          />
-          <Modal.Open opens='experience'>
-            <button className='bg-dark-2 text-light-2 border-t-[1px] py-1.5 font-semibol border-dark-1-border rounded-b-sm w-full absolute bottom-0 left-0 lg:text-xl lg:py-3'>
-              View more
-            </button>
-          </Modal.Open>
-        </Card>
-        <Card>
-          <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>
-            Skills
-          </h3>
-          <ItemsList
-            items={skills?.slice(0, 5)}
-            render={(el, i) => <SkillItem el={el} i={i} key={`${el}-${i}`} />}
-          />
-          <Modal.Open opens='skills'>
-            <button className='bg-dark-2 text-light-2 border-t-[1px] py-1.5 font-semibol border-dark-1-border rounded-b-sm w-full absolute bottom-0 left-0 lg:text-xl lg:py-3'>
-              View more
-            </button>
-          </Modal.Open>
-        </Card>
-        <Modal.Window windowName='experience' title='Experiences'>
+        {experience.lenght > 1 && (
+          <Card>
+            <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>
+              Experience & Projects
+            </h3>
+            <ItemsList
+              items={experience?.slice(0, 3)}
+              render={el => (
+                <ExperienceItem
+                  el={el}
+                  key={el._id}
+                />
+              )}
+            />
+            <Modal.Open opens='experience'>
+              <button className='bg-dark-2 text-light-2 border-t-[1px] py-1.5 font-semibol border-dark-1-border rounded-b-sm w-full absolute bottom-0 left-0 lg:text-xl lg:py-3'>
+                View more
+              </button>
+            </Modal.Open>
+          </Card>
+        )}
+        {skills.lenght > 1 && (
+          <Card>
+            <h3 className='text-light-2 font-semibold mb-2 lg:text-2xl'>
+              Skills
+            </h3>
+            <ItemsList
+              items={skills?.slice(0, 5)}
+              render={(el, i) => (
+                <SkillItem
+                  el={el}
+                  i={i}
+                  key={`${el}-${i}`}
+                />
+              )}
+            />
+            <Modal.Open opens='skills'>
+              <button className='bg-dark-2 text-light-2 border-t-[1px] py-1.5 font-semibol border-dark-1-border rounded-b-sm w-full absolute bottom-0 left-0 lg:text-xl lg:py-3'>
+                View more
+              </button>
+            </Modal.Open>
+          </Card>
+        )}
+        <Modal.Window
+          windowName='experience'
+          title='Experiences'
+        >
           <ItemsList
             items={experience}
-            render={el => <ExperienceItem el={el} key={el._id} />}
+            render={el => (
+              <ExperienceItem
+                el={el}
+                key={el._id}
+              />
+            )}
           />
         </Modal.Window>
-        <Modal.Window windowName='skills' title='Skills'>
+        <Modal.Window
+          windowName='skills'
+          title='Skills'
+        >
           <ItemsList
             items={skills}
-            render={(el, i) => <SkillItem el={el} i={i} />}
+            render={(el, i) => (
+              <SkillItem
+                el={el}
+                i={i}
+              />
+            )}
           />
         </Modal.Window>
       </Modal>
