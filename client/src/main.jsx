47a1270ebad953d554bc1ea4c60dbee1b3808b50
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-15 19:43:00 -0300

    Error boundary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <ErrorBoundary FallbackComponent={<h1 className='text-dark-1'>ERROR</h1>}> */}
     <App />
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 );
