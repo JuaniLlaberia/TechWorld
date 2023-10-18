88ffeca441c70eb1450bbaa563684019b714585f
Juan Ignacio Llaberia <99926853+JuaniLlaberia@users.noreply.github.com>
2023-10-18 12:45:53 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,8 +19,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(
   cors({
-    //origin: 'http://localhost:5173',
-    origin: 'https://techworld-jobs.vercel.app/',
+    origin: 'https://techworld-jobs.vercel.app',
     credentials: true,
   })
 );
