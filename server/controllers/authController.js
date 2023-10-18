7548d719a9eefb7b05b2bdfba7c1c753e8413b52
Juan Ignacio Llaberia <99926853+JuaniLlaberia@users.noreply.github.com>
2023-10-18 13:08:21 -0300

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,6 +19,8 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
 
+  console.log(token);
+  
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 1000
@@ -26,6 +28,8 @@ const createSendToken = (user, statusCode, res) => {
     path: '/',
     httpOnly: true,
   };
+
+  console.log(cookieOptions);
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
