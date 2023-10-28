7a7fff7f4044c92aac88aaf205ab261d92a222d5
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-28 17:24:58 -0300

    Fixed tiptap styling bugs

--- client/src/components/Tiptap.jsx
+++ client/src/components/Tiptap.jsx
@@ -21,7 +21,7 @@ const Tiptap = () => {
     editorProps: {
       attributes: {
         class:
-          'min-h-[80vh] [&>p]:text-base mb-3 rounded-md bg-transparent text-light-1 outline-none py-2 [&>pre]:bg-dark-2 [&>pre]:text-light-1 [&>pre]:text-[0.8rem] [&>pre]:rounded-md [&>pre]:p-2 [&>pre]:m-3 [&>h1]:text-3xl [&>h1]:font-semibold [&>h2]:text-xl [&>h2]:font-semibold [&>ul]:px-6 [&>ul]:list-disc lg:[&>h1]:text-3xl xl:[&>h1]:text-4xl lg:[&>h2]:text-2xl lg:[&>p]:text-lg xl:[&>p]:text-xl lg:[&>pre]:text-[1rem] xl:[&>pre]:text-[1.2rem] lg:[&>pre]:p-3 xl:[&>pre]:p-4 [&>pre]:overflow-x-auto lg:w-[70vw] xl:w-[40vw]',
+          'min-h-[80vh] [&>p]:text-base mb-3 rounded-md bg-transparent text-light-1 outline-none py-2 [&>pre]:bg-dark-2 [&>pre]:text-light-1 [&>pre]:text-[0.8rem] [&>pre]:rounded-md [&>pre]:p-2 [&>pre]:m-3 [&>h1]:text-2xl [&>h1]:font-semibold [&>h2]:text-xl [&>h2]:font-semibold [&>ul]:px-6 [&>ul]:list-disc lg:[&>h1]:text-3xl xl:[&>h1]:text-4xl lg:[&>h2]:text-2xl lg:[&>p]:text-lg xl:[&>p]:text-xl lg:[&>pre]:text-[1rem] xl:[&>pre]:text-[1.2rem] lg:[&>pre]:p-3 xl:[&>pre]:p-4 [&>pre]:overflow-x-auto lg:w-[70vw] xl:w-[40vw]',
       },
     },
     onUpdate({ editor }) {
