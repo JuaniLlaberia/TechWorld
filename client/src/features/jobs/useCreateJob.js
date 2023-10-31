7bb8640636cccb6488a0555e9a59e200c8230605
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-31 11:57:32 -0300

    Fixed bug in custom hook

--- client/src/features/jobs/useCreateJob.js
+++ client/src/features/jobs/useCreateJob.js
@@ -1,15 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
 import { newJob } from '../../api/jobApi';
 
 export const useCreateJob = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: createJob, isLoading: isCreating } = useMutation({
     mutationFn: body => newJob(body),
     onSuccess: data => {
       toast.success('Job created');
+      queryClient.invalidateQueries({ queryKey: ['my-jobs'] });
       navigate(`/jobs/all?currentJobId=${data.data.job._id}`);
     },
     onError: err => toast.error(err.message),
