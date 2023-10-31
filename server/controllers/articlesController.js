c6e5f130d78a7ce3ff014e4717669958eb382985
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-10-31 11:56:38 -0300

    Implemented articles update code  in server

--- server/controllers/articlesController.js
+++ server/controllers/articlesController.js
@@ -25,6 +25,7 @@ exports.getArticles = catchErrorAsync(async (req, res) => {
 
   res.status(200).json({
     status: 'success',
+    count: articles.length,
     pages: Math.ceil(totalArticles / process.env.PAGE_SIZE),
     data: { articles },
   });
@@ -57,11 +58,25 @@ exports.getMyArticles = catchErrorAsync(async (req, res) => {
 
   res.status(200).json({
     status: 'success',
+    count: articles.length,
     data: { articles },
   });
 });
 
-exports.updateArticle = catchErrorAsync(async () => {});
-exports.deleteArticle = catchErrorAsync(async () => {});
-exports.likeArticle = catchErrorAsync(async () => {});
-exports.unLikeArticle = catchErrorAsync(async () => {});
+exports.updateArticle = catchErrorAsync(async (req, res) => {
+  let filteredObj = { ...req.body };
+  const fieldsToRemove = ['_id', 'createdAt', 'author'];
+  fieldsToRemove.forEach(el => delete filteredObj[el]);
+
+  const article = await Article.findByIdAndUpdate(req.params.id, filteredObj, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ status: 'success', data: { article } });
+});
+
+exports.deleteArticle = catchErrorAsync(async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.status(200).json({ status: 'success', data: null });
+});
