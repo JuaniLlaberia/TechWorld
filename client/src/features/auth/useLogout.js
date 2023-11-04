31f9ea3aed95e21de9ab4fc61fe158c8eea23521
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-04 15:02:26 -0300

    Added page redirection with logout

--- client/src/features/auth/useLogout.js
+++ client/src/features/auth/useLogout.js
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout as logoutAPI } from '../../api/authApi';
+import { useNavigate } from 'react-router-dom';
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const { mutate: logout, isLoading } = useMutation({
     mutationFn: logoutAPI,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['user'] });
+      navigate('/');
     },
   });
 
