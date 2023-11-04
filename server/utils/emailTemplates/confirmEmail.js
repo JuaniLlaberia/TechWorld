bb8b579927c7ba7694b636b2de642668dc00a6a7
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-04 15:16:13 -0300

    Updated emails template

--- server/utils/emailTemplates/confirmEmail.js
+++ server/utils/emailTemplates/confirmEmail.js
@@ -5,7 +5,6 @@ module.exports = createEmailConfirmTemplate = url => {
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Simple Transactional Email</title>
     <style>
         @media only screen and (max-width: 620px) {
         table.body h1 {
